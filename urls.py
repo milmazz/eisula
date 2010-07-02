575c19f511c77f735f6d0d97f6af79bc3a880bc0
Milton Mazzarri <milmazz@gmail.com>
2010-07-02 01:39:37 -0430

    Ajuste inicial de las URL del proyecto

--- urls.py
+++ urls.py
@@ -1,20 +1,24 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
-from django.views.static import serve
+from django.views.static import server
+
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^miembros/', include('eisula.miembros.urls_miembros')),
-    (r'^dpto/', include('eisula.miembros.urls_dpto')),
-    (r'^grupo/', include('eisula.miembros.urls_grupo')),
-    (r'^publicacion/', include('eisula.miembros.urls_publicacion')),
-    (r'^noticias/', include('eisula.blog.urls')),
-    (r'^pensum/', include('eisula.pensum.urls')),
-    (r'^pg/', include('eisula.pg.urls')),
+    #(r'^miembros/', include('eisula.miembros.urls_miembros')),
+    #(r'^dpto/', include('eisula.miembros.urls_dpto')),
+    #(r'^grupo/', include('eisula.miembros.urls_grupo')),
+    #(r'^publicacion/', include('eisula.miembros.urls_publicacion')),
+    #(r'^noticias/', include('eisula.blog.urls')),
+    #(r'^pensum/', include('eisula.pensum.urls')),
+    #(r'^pg/', include('eisula.pg.urls')),
     (r'^faq/', include('eisula.faq.urls')),
-    (r'^solicitudes/', include('eisula.solicitudes.urls')),
-    (r'^horarios/', include('eisula.horariosMaterias.urls')),
-    (r'^comments/', include('django.contrib.comments.urls.comments')),
-    (r'^admin/', include('django.contrib.admin.urls')),
+    #(r'^solicitudes/', include('eisula.solicitudes.urls')),
+    #(r'^horarios/', include('eisula.horariosMaterias.urls')),
+    #(r'^comments/', include('django.contrib.comments.urls.comments')),
+    #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include('admin.site.urls')),
 )
 
 #Servir ficheros estaticos
